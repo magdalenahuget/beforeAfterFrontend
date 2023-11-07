75fd96c7bcc86e72d9c8711bf61a288052a96eac
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-11-07 16:54:41 +0100

    Feat: Create Header component with AppBar and Typography

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './components/Main';
+import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <Header />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
