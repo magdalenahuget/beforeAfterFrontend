4eb7db7fc8d5ff508cfac94e7158189e0547e853
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-11-07 17:32:29 +0100

    Feat: Implement ImageDetails with refactored Header and BottomNav - Created ImageDetails component integrating Header and BottomNav. - Refactored BottomNav and Header from function to const arrow functions

---
+++ src/components/ImageDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import BottomNav from './BottomNav';
+
+const ImageDetails = () => {
+    return (
+        <>
+            <Header />
+            <img src="https://i.ytimg.com/vi/EU0unRN6BdU/maxresdefault.jpg" alt="before-and-after" />
+            <BottomNav />
+        </>
+    );
+};
+
+export default ImageDetails;
