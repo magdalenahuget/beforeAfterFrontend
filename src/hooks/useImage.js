b220b5b057ff515fcb775d3c77dfe3e1f8394410
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-11-09 20:28:05 +0100

    Feat: useImage added

---
+++ src/hooks/useImage.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { imageDataApi } from '../api/imageApi';
+
+const useImage = (imageId) => {
+    const [imageData, setImageData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            if (!imageId) return;
+
+            setIsLoading(true);
+            try {
+                const response = await imageDataApi.getImageDescriptionById(imageId);
+                setImageData(response.data);
+            } catch (error) {
+                console.error('Error fetching image data:', error);
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        })();
+    }, [imageId]);
+
+    return { imageData, isLoading, error };
+};
+
+export default useImage;
