36a9cfb0907c90f86a851b3590a006bc0ca0ed14
magdalenahuget <magdalena.huget@gmail.com>
2023-11-14 20:44:55 +0100

    Feature: add useEffect hook to call cities fetch
    - Fetch called when selectedCity state changes,
    only if length is grater than zero
    - Add variable to .env file

--- src/components/image/CitySearch.jsx
+++ src/components/image/CitySearch.jsx
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Box from "@mui/material/Box";
 import axios from "axios";
 import Autocomplete from '@mui/material/Autocomplete';
+import {TextField} from "@mui/material";
+
 
 const CitySearch = () => {
+    const API_KEY = `${process.env.REACT_APP_CITIES_API_KEY}`;
     const URL_CITY_SEARCH = `http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=`;
     const [cities, setCities] = useState([]);
     const [selectedCity, setSelectedCity] = useState('');
@@ -24,6 +27,12 @@ const CitySearch = () => {
         }
     };
 
+    useEffect(() => {
+        if (selectedCity.length > 0) {
+            fetchCitiesFromApi(selectedCity);
+        }
+    }, [selectedCity]);
+
     return (
         <Box sx={{marginBottom: 2, width: '100%'}}>
             <Autocomplete
@@ -31,6 +40,13 @@ const CitySearch = () => {
                 id="combo-box"
                 options={cities}
                 sx={{width: 300}}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="City"
+                        onChange={(event) => fetchCitiesFromApi(event.target.value)}
+                    />
+                )}
                 onChange={handleSelectedCity}
             />
         </Box>
