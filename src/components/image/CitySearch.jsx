aa238dfcac7bb446c48da2019aa2036868dda80d
magdalenahuget <magdalena.huget@gmail.com>
2023-11-14 20:15:38 +0100

    Feature: fetch cities from wheather api
    - Implement a method for fetching data from api

--- src/components/image/CitySearch.jsx
+++ src/components/image/CitySearch.jsx
@@ -1,31 +1,40 @@
 import React, {useState} from 'react';
 import Box from "@mui/material/Box";
-import {TextField} from "@mui/material";
+import axios from "axios";
 import Autocomplete from '@mui/material/Autocomplete';
 
 const CitySearch = () => {
-        const [cities, setCities] = useState([]);
-        const [selectedCity, setSelectedCity] = useState('');
+    const URL_CITY_SEARCH = `http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=`;
+    const [cities, setCities] = useState([]);
+    const [selectedCity, setSelectedCity] = useState('');
 
-        // CITY
-        const handleSelectedCity = (event) => {
-            setSelectedCity(event.target.value);
-        };
+    const handleSelectedCity = (event) => {
+        setSelectedCity(event.target.value);
+    };
 
+    const fetchCitiesFromApi = async (query) => {
+        try {
+            const response = await axios.get(`${URL_CITY_SEARCH}${query}`);
+            const citiesData = response.data;
+            console.log(response.data);
+            const cityNames = citiesData.map((location) => location.name);
+            setCities(cityNames);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
-        return (
-            <Box sx={{marginBottom: 2, width: '100%'}}>
-                <Autocomplete
-                    disablePortal
-                    id="combo-box"
-                    options={cities}
-                    sx={{ width: 300 }}
-                    renderInput={(params) => <TextField {...params} label="City" />}
-                    onChange={handleSelectedCity}
-                />
-            </Box>
-        );
-    }
-;
+    return (
+        <Box sx={{marginBottom: 2, width: '100%'}}>
+            <Autocomplete
+                disablePortal
+                id="combo-box"
+                options={cities}
+                sx={{width: 300}}
+                onChange={handleSelectedCity}
+            />
+        </Box>
+    );
+};
 
 export default CitySearch;
