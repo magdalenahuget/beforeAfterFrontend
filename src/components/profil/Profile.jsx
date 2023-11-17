7f1efda037d100638ca7daa817aa9af18e7e3b64
Marcin Kącki <kacki.marcin@gmail.com>
2023-11-17 13:11:39 +0100

    feat: Implement user profile functionality
    Refactor and implement user profile components, including the ContactDetailsForm and AboutMeForm. The Profile component now manages state for contact details, about me section, and form data. The logic for fetching and updating user details has been modularized and optimized.
    
    Changes:
    - Added ContactDetailsForm and AboutMeForm components for user profile editing.
    - Refactored Profile component to manage state for contact details, about me, and form data.
    - Utilized useEffect to fetch contact details and about me data on component mount.
    - Implemented handlers for tab changes, form input changes, and form submissions.
    - Validated postcode and phone number formats in the ContactDetailsForm.
    - Incorporated logic to update and submit contact details and about me data.
    
    TODOs:
    - Implement actual logic for form submissions in handleSubmitForm and handleSubmitAboutMe functions.
    
    Note: The current TODOs indicate that the logic for form submissions is yet to be implemented. Ensure to add the necessary backend logic for data submission.

--- src/components/profil/Profile.jsx
+++ src/components/profil/Profile.jsx
@@ -1,15 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import Box from "@mui/material/Box";
 import TabPanel from "../offer/tabs/TabPanel";
-import AboutMe from "../user/AboutMe";
 import {contactDetailsApi} from "../../api/contactDetailsApi";
 import BottomNav from "../layout/BottomNav";
 import Header from "../layout/Header";
 import ContactDetailsForm from "./ContactDetailsForm";
 import ProfileTabs from "./ProfileTabs";
-
+import {getAboutMeByUserId} from "../../api/userApi";
+import AboutMeForm from "./AboutMeForm";
 
 const Profile = ({userId}) => {
+
     const [formData, setFormData] = useState({
         streetName: '',
         streetNumber: '',
@@ -19,65 +20,110 @@ const Profile = ({userId}) => {
         webpage: '',
         cityName: '',
     })
-
+    const [updateAboutMe, setUpdateAboutMe] = useState("")
     const [contactDetails, setContactDetails] = useState([])
+    const [aboutMe, setAboutMe] = useState('')
     const [value, setValue] = useState(0);
 
     useEffect(() => {
+
         contactDetailsApi.getContactDetailsByUserId(userId)
             .then(response => setContactDetails(response.data))
     }, [userId]);
 
+    useEffect(() => {
+
+        getAboutMeByUserId(userId)
+            .then(response => setAboutMe(response.data))
+    }, [userId]);
+
     const handleTabChange = (event, newValue) => {
+
         setValue(newValue);
+        if (newValue === 1) {
+            setUpdateAboutMe({
+                aboutMe: aboutMe.aboutMe,
+            });
+        }
     };
 
     const handleFormInputChange = (event) => {
-        const { name, value } = event.target;
+
+        const {name, value} = event.target;
         const modifiedValue = name === 'phoneNumber' ? value.replace(/\D/g, '').slice(0, 9) : value;
 
         setFormData((prevFormData) => ({
             ...prevFormData,
             [name]: modifiedValue,
         }));
+
         setContactDetails((prevContactDetails) => ({
             ...prevContactDetails,
             [name]: modifiedValue,
         }));
     };
 
-    const handleSubmit = (event) => {
+    const handleAboutMeChange = (event) => {
+
+        const {value} = event.target;
+        setUpdateAboutMe((prevUpdateAboutMe) => ({
+            ...prevUpdateAboutMe,
+            aboutMe: value,
+        }));
+    };
+    const handleSubmitForm = (event) => {
+
         event.preventDefault();
 //TODO logika do wysylania
         const phoneNumberWithCountryCode = `+48${formData.phoneNumber}`;
 
-        console.log("test" + contactDetails.streetName)
+    };
+    const handleSubmitAboutMe = (event) => {
+
+        event.preventDefault();
+        // TODO: Logika do wysyłania
+
+        setAboutMe((prevAboutMe) => ({
+            ...prevAboutMe,
+            aboutMe: updateAboutMe.aboutMe || aboutMe.aboutMe,
+        }));
+
     };
 
     const isPostcodeValid = () => {
+
         const postcodePattern = /^\d{2}-\d{3}$/;
         return postcodePattern.test(formData.postcode);
     };
 
     const isPhoneNumberValid = () => {
+
         const phoneNumberPattern = /^\d{9}$/;
         return phoneNumberPattern.test(formData.phoneNumber) && !formData.phoneNumber.startsWith('+48');
     };
 
-    console.log(contactDetails.postcode)
-
     return (
         <>
-            <Header />
-            <Box sx={{ width: '100%', mt: '4vh', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '50px -0px' }}>
-                <ProfileTabs value={value} handleTabChange={handleTabChange} />
-
+            <Header/>
+            <Box sx={{
+                width: '100%',
+                mt: '4vh',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                margin: '50px -0px'
+            }}>
+                <ProfileTabs value={value} handleTabChange={handleTabChange}/>
                 {value === 1 && (
                     <TabPanel value={value} index={1}>
-                        <AboutMe userId={userId} />
+                        <AboutMeForm
+                            aboutMe={aboutMe}
+                            updateAboutMe={updateAboutMe}
+                            handleAboutMeChange={handleAboutMeChange}
+                            handleSubmitAboutMe={handleSubmitAboutMe}
+                        />
                     </TabPanel>
                 )}
-
                 {value === 2 && (
                     <TabPanel value={value} index={2}>
                         <ContactDetailsForm
@@ -85,13 +131,13 @@ const Profile = ({userId}) => {
                             isPostcodeValid={isPostcodeValid}
                             isPhoneNumberValid={isPhoneNumberValid}
                             handleFormInputChange={handleFormInputChange}
-                            handleSubmit={handleSubmit}
+                            handleSubmitForm={handleSubmitForm}
                             contactDetails={contactDetails}
                         />
                     </TabPanel>
                 )}
             </Box>
-            <BottomNav />
+            <BottomNav/>
         </>
 
     );
