6007052dc9700a6bba4122a6d99ab0c1ce992f5d
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-11-20 19:06:07 +0100

    Ref: sending favourite image to the database

--- src/components/image/ImageCard.jsx
+++ src/components/image/ImageCard.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {useLocation} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import IconButton from '@mui/material/IconButton';
@@ -9,29 +9,30 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Box from '@mui/material/Box';
 import useFavourites from "../../hooks/useFavourites";
 
-const ImageCard = ({image, onDeleteImage}) => {
+const ImageCard = ({ image, onDeleteImage }) => {
     const location = useLocation();
-    const {favourites, addFavourite, removeFavourite} = useFavourites(image.userId);
+    const { favourites, addFavourite, removeFavourite, isLoading } = useFavourites(image.userId);
     const [isFavourite, setIsFavourite] = useState(false);
 
     useEffect(() => {
-        setIsFavourite(favourites.some(fav => fav.id === image.id));
-    }, [favourites, image.id]);
+        if (!isLoading) {
+            setIsFavourite(favourites.some(fav => fav.id === image.id));
+        }
+    }, [favourites, image.id, isLoading]);
 
     const handleToggleFavourite = () => {
         if (isFavourite) {
-            removeFavourite(image.id);
+            removeFavourite(image.id).then(() => setIsFavourite(false));
         } else {
-            addFavourite(image.id);
+            addFavourite(image.id).then(() => setIsFavourite(true));
         }
-        setIsFavourite(!isFavourite);
     };
 
     const showFavoriteIcon = location.pathname === '/home' || location.pathname === '/offer';
     const showDeleteIcon = location.pathname === '/images' || location.pathname === '/favourites';
 
     return (
-        <Card sx={{position: 'relative', height: '100%', display: 'flex', flexDirection: 'column'}}>
+        <Card sx={{ position: 'relative', height: '100%', display: 'flex', flexDirection: 'column' }}>
             <CardMedia
                 component="img"
                 image={image.url}
@@ -42,14 +43,15 @@ const ImageCard = ({image, onDeleteImage}) => {
                     objectFit: 'cover'
                 }}
             />
-            <Box sx={{position: 'absolute', top: 0, right: 0}}>
+            <Box sx={{ position: 'absolute', top: 0, right: 0 }}>
                 {showFavoriteIcon && (
                     <IconButton
                         aria-label={isFavourite ? 'remove from favorites' : 'add to favorites'}
                         onClick={handleToggleFavourite}
+                        disabled={isLoading}
                     >
-                        {isFavourite ? <FavoriteIcon sx={{color: 'purple', fontSize: '1.2em'}}/> :
-                            <FavoriteBorderIcon sx={{color: 'purple', fontSize: '1.2em'}}/>}
+                        {isFavourite ? <FavoriteIcon sx={{ color: 'purple', fontSize: '1.2em' }} /> :
+                            <FavoriteBorderIcon sx={{ color: 'purple', fontSize: '1.2em' }} />}
                     </IconButton>
                 )}
                 {showDeleteIcon && (
@@ -57,7 +59,7 @@ const ImageCard = ({image, onDeleteImage}) => {
                         aria-label="delete image"
                         onClick={() => onDeleteImage(image.id)}
                     >
-                        <DeleteIcon sx={{color: 'purple', fontSize: '1.2em'}}/>
+                        <DeleteIcon sx={{ color: 'purple', fontSize: '1.2em' }} />
                     </IconButton>
                 )}
             </Box>
