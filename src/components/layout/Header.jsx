cfef04b9ac869abbd9ad9078b79db67ea9d2ef27
magdalenahuget <magdalena.huget@gmail.com>
2023-11-22 23:51:39 +0100

    Feature: add logout implementation
    - Add display logout icon if user logged
    - Remove jwt token from session storage after logout

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Typography from '@mui/material/Typography';
+import LogoutIcon from '@mui/icons-material/Logout';
+import {getAboutMeByUserId} from "../../api/userApi";
+import Fab from "@mui/material/Fab";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import {useNavigate} from "react-router-dom";
 
 const Header = () => {
     const navigate = useNavigate();
 
+    const [isUserLogged, setIsUserLogged] = useState(false);
+
+    useEffect(() => {
+        console.log(sessionStorage.getItem("jwt"))
+        if (sessionStorage.getItem("jwt")) {
+            console.log('true if')
+            setIsUserLogged(true);
+        } else {
+            console.log('else')
+            console.log(isUserLogged);
+            setIsUserLogged(false);
+        }
+
+    }, [sessionStorage.getItem("jwt")]);
+
+    const handleLogout = () => {
+        console.log('handle logout')
+        sessionStorage.removeItem("jwt");
+        navigate('/');
+    }
+
+
     return (
         <AppBar position="fixed">
             <Toolbar>
-                <CameraIcon sx={{ mr: 2 }} onClick={() => navigate('/')} />
-                <Typography onClick={() => navigate('/')} variant="h6" color="inherit" noWrap>
+                <CameraIcon sx={{mr: 2}}/>
+                <Typography variant="h6" color="inherit" noWrap>
                     Before And After
                 </Typography>
+                {isUserLogged && (
+                    <Fab color="primary" aria-label="home" onClick={handleLogout} sx={{marginLeft: 'auto'}}>
+                        <LogoutIcon onClick={handleLogout}  />
+                    </Fab>
+                )}
             </Toolbar>
-        </AppBar>
-    );
+        </AppBar>);
 };
 
 export default Header;
